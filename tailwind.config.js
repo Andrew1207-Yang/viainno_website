eecf34fa3e4d95f21d361e5512188232f6fa9069
Andrew1207-Yang <a.yang353188@gmail.com>
2023-08-02 09:11:35 +0800

    added index page'

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         theme_green: '#2e9189',
+        dark_green: '#3F4D4C',
         dark_gray: '#1c1c1c',
+        light_gray: '#d1d1d1'
       },
       fontFamily:{
         noto: ['Noto Sans TC', 'sans-serif'],
